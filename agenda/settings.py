ec4198368621a0fe9a2f3af1fd272ada8e0f5b44
Pedro <phdss04@gmail.com>
2022-11-16 15:22:05 -0300

    some changes

--- agenda/settings.py
+++ agenda/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,9 +135,8 @@ MESSAGE_TAGS = {
     constants.INFO: 'alert-info'
 } 
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
