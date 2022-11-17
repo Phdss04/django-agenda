f775334857ff9503d0b431f981f77fa3e21e9db9
Pedro <phdss04@gmail.com>
2022-11-17 13:24:06 -0300

    hiding the secret key

--- agenda/settings.py
+++ agenda/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-he+*=tdko@=kkq%h5p3n3vjwb@ra86fpt(v__y!#&-f!nj5pg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
