3c0321b3c89797d22d7726a867db65ac139a1d0e
Pedro <phdss04@gmail.com>
2022-11-17 22:41:08 -0300

    changes on notebook

--- agenda/settings.py
+++ agenda/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_on_heroku
 from decouple import config
-
-
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-agenda-projeto.herokuapp.com']
+ALLOWED_HOSTS = ['django-agenda-projeto.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -122,6 +120,8 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -147,4 +147,3 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_on_heroku.settings(locals())
