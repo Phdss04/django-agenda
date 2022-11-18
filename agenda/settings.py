3f0de10296148ad3c0c7a7fc98c0dd9b78fae5e5
Pedro <phdss04@gmail.com>
2022-11-18 13:00:46 -0300

    commiting to the new repository

--- agenda/settings.py
+++ agenda/settings.py
@@ -17,15 +17,16 @@ from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#print(get_random_secret_key())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['django-agenda-projeto.herokuapp.com', '127.0.0.1']
 
