a543fdfd467631f0e35a393fe12a8ae1e7af7aca
Pedro <phdss04@gmail.com>
2022-11-18 13:48:28 -0300

    make it better

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
